3ff959e15560428a58ebcd04ad266724b87814eb
Ilya Vanyaev <IL4Van@yandex.ru>
2022-12-31 22:16:49 +0300

    fixed

--- src/pr27/Main.java
+++ src/pr27/Main.java
@@ -1,6 +1,5 @@
 package pr27;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,15 +8,15 @@ public class Main {
     public static void main(String[] args) {
         Map<String, String> nameMap = new HashMap<>();
         nameMap.put("Smith", "John");
-        nameMap.put("Johnson", "Mike");
-        nameMap.put("Williams", "Kate");
+        nameMap.put("Walker", "Paul");
+        nameMap.put("Diesel", "Vin");
         nameMap.put("Jones", "John");
-        nameMap.put("Brown", "Anne");
-        nameMap.put("Davis", "Tom");
-        nameMap.put("Miller", "Chris");
-        nameMap.put("Wilson", "Kate");
-        nameMap.put("Moore", "Bill");
-        nameMap.put("Taylor", "Anne");
+        nameMap.put("Brown", "James");
+        nameMap.put("Taylor", "Tom");
+        nameMap.put("Rock", "Chris");
+        nameMap.put("Hudson", "Kate");
+        nameMap.put("Milligan", "Bill");
+        nameMap.put("Boleyn", "Anne");
 
         // удаление людей с одинаковыми именами
         nameMap.values().removeIf(name -> Collections.frequency(nameMap.values(), name) > 1);
